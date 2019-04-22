e7c1e63c3b1d8a32ecf37492331e7cf428824aa5
a11exandru <alexpuiu20@yahoo.com>
2019-04-22 17:15:32 +0300

    Add smooth transtion to # links and animation to down arrow

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ window.addEventListener('scroll', () => {
 	let logo = document.querySelector('.logo-image');
 	let menuLinks = document.querySelector('.menu-links');
 	let animatie = document.querySelector('.animation-text');
-	header.style.background = 'linear-gradient(to right, #141E30, #243B55)';
+	let toTop = document.querySelector('.toTop');
+	// header.style.background = 'linear-gradient(to right, #141E30, #243B55)';
+	header.style.background = '#111';
 	logo.style.height = '60px';
 	logo.style.width = '130px';
 	animatie.classList.add('apareDefinitia');
+
+	if (window.scrollY > 150) {
+		toTop.style.opacity = "1";
+	}
+
+	if(window.scrollY < 150) {		
+		toTop.style.opacity = "0";
+	}
+
+	if(window.scrollY < 50) {
+		header.style.background = 'linear-gradient(to left, #09203f  , #537895)';
+		logo.style.height = '70px';
+		logo.style.width = '170px';
+	}
 })
